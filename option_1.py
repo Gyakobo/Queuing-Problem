21816faf00f1328a97acfc502e91cdf28805b275
Gyakobo <andygyakobo@gmail.com>
2024-06-13 18:41:37 -0700

    Attempting to solve option 1

--- option_1.py
+++ option_1.py
@@ -10,7 +10,44 @@ number_of_stations = 5
 
 # Generate arrival and service times
 def generate_arrivals(duration, arrival_rate):
-    arrival = []
+    arrivals = []
     time = 0
 
+    while time < duration:
+        time += np.random.exponential(arrival_rate)
+        arrivals.append(time)
+    return arrivals
 
+def generate_service_time(service_rate):
+    return np.random.exponential(service_rate)
+
+# Option 1 - Single queue for all service stations
+# Assuming we are given 5 servers to process the single queue
+def single_queue(arrivals, service_rate, number_of_stations):
+    queue           = []
+    servers         = [0] * number_of_stations
+    waiting_times   = [] 
+    waiting_time    = 0
+
+    for arrival_time in arrivals:
+        heapq.heappush(queue, (arrival_time, generate_service_time(service_rate)))
+
+    while queue:
+        arrival_time, service_time = heapq.heappop(queue)
+        next_free_time = min(servers)
+        if next_free_time > arrival_time:
+            waiting_time = next_free_time - arrival_time 
+        else:
+            waiting_time = 0
+        waiting_times.append(waiting_time)
+        next_free_time = max(next_free_time, arrival_time) + service_time
+        servers[servers.index(min(servers))] = next_free_time
+
+    return np.mean(waiting_time)
+
+
+# Run simulation
+arrivals = generate_arrivals(D, A)
+average_waiting_time_1 = single_queue(arrivals, S, number_of_stations)
+
+print(f'Option 1: Average waiting time for "Single Queue" {average_waiting_time_1} minutes')
